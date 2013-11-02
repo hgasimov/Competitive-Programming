013552559a3da4cc28465d24e5520f276003e3d8
hgasimov <gasimov.huseyn@gmail.com>
2013-11-02 18:01:14 +0100

    My first solutions at topcoder

--- training/LittleElephantAndIntervalsDiv1.java
+++ training/LittleElephantAndIntervalsDiv1.java
@@ -1,11 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topcoder;
 
-//import java.util.TreeSet;
 /**
  *
  * @author huseyngasimov
