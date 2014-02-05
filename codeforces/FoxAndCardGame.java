df6d77aaccf2c6226162001669e0526b644ba7d9
hgasimov <gasimov.huseyn@gmail.com>
2014-02-05 19:24:18 +0100

    Codeforces Round #228 (Div 2) - FoxAndCardGame - Deck Implementation

--- codeforces/FoxAndCardGame.java
+++ codeforces/FoxAndCardGame.java
@@ -9,25 +9,23 @@ import java.util.Scanner;
  * (does not pass all the system tests)
  */
 public class FoxAndCardGame {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         
         int N = in.nextInt();        
-        Object[] x = new Object[N];       
-        int[][] ends = new int[N][2];        
+        Deck[] x = new Deck[N];        
         PriorityQueue<Index> topMaxPQ = new PriorityQueue<Index>();
         PriorityQueue<Index> bottomMaxPQ = new PriorityQueue<Index>();
         
         for (int i = 0; i < N; i++) {      
             int si = in.nextInt();
-            ends[i][1] = si - 1;
             
             int[] c = new int[si];
             for (int j = 0; j < si; j++)
                 c[j] = in.nextInt();
             
-            x[i] = c;            
+            x[i] = new Deck(c);            
             
             topMaxPQ.add(new Index(i, c[0]));
             bottomMaxPQ.add(new Index(i, c[si-1]));
@@ -37,8 +35,8 @@ public class FoxAndCardGame {
         int jiro = 0;
         
         while (topMaxPQ.size() > 0 || bottomMaxPQ.size() > 0) {
-            ciel += remove(topMaxPQ, x, ends, 0);
-            jiro += remove(bottomMaxPQ, x, ends, 1);
+            ciel += remove(topMaxPQ, x, true);
+            jiro += remove(bottomMaxPQ, x, false);
         }
         
         
@@ -46,29 +44,25 @@ public class FoxAndCardGame {
         out.flush();        
     }
     
-    private static int remove(PriorityQueue<Index> pq, Object[] x, int[][] ends, int j) {
+    private static int remove(PriorityQueue<Index> pq, Deck[] x, boolean removeFromTop) throws Exception {
         if (pq.size() <= 0) return 0;
-        Index max = pq.poll();
         
-        int i = max.i;
+        int i = pq.poll().i;
         int sum = 0;
         
         // if the chosen pile is empty try another piles
-        while (ends[i][0] > ends[i][1] && pq.size() > 0) {
-            max = pq.poll();
-            i = max.i;
+        while (x[i].isEmpty() && pq.size() > 0) {
+            i = pq.poll().i;
         }
         
         // if the card has not been taken by another player
-        if (ends[i][0] <= ends[i][1]) {
-            sum = max.val;    
-            
-            int newEnd = (j == 0) ? ++ends[i][0] : --ends[i][1];
+        if (!x[i].isEmpty()) {
+            sum = (removeFromTop) ? x[i].removeFromTop() : x[i].removeFromBottom();            
             
             // if there're still cards in this pile
-            if (ends[i][0] <= ends[i][1]) {
-                int[] c = (int[]) x[i];
-                pq.add(new Index(i, c[newEnd]));
+            if (!x[i].isEmpty()) {
+                int tmp = (removeFromTop) ? x[i].top() : x[i].bottom();
+                pq.add(new Index(i, tmp));
             }
         }
         
@@ -90,6 +84,42 @@ public class FoxAndCardGame {
         @Override
         public int compareTo(Index t) {
             return t.val - this.val;
+        }
+    }
+    
+    private static class Deck {
+        private int[] a;
+        private int N, i, j;
+        
+        public Deck(int[] a) {
+            this.a = a;
+            N = a.length;
+            i = 0;
+            j = N-1;
+        }
+        
+        public boolean isEmpty() {
+            return i > j;
+        }
+        
+        public int top() throws Exception {
+            if (isEmpty()) throw new Exception("Deck is empty");
+            return a[i];
+        }
+        
+        public int bottom() throws Exception {
+            if (isEmpty()) throw new Exception("Deck is empty");
+            return a[j];
+        }
+        
+        public int removeFromTop() throws Exception {
+            if (isEmpty()) throw new Exception("Deck is empty");
+            return a[i++];
+        }
+        
+        public int removeFromBottom() throws Exception {
+            if (isEmpty()) throw new Exception("Deck is empty");
+            return a[j--];
         }
     }
 }
